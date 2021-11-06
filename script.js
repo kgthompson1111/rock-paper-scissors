999882756dba9cf41a4dd5d22f4248528e289ba8
kgthompson1111 <kgthompson@null.net>
2021-11-05 21:11:15 -0600

    Added a GUI

---
+++ script.js
@@ -0,0 +1,85 @@
+ // declare two variables - the computer's guess, and the human's guess
+ let computerGuess, humanGuess;
+ let computerScore = 0;
+ let humanScore = 0;
+ let winner = false;
+
+ function computerPlay() {
+     let randomizedGuess = Math.floor(Math.random() * 3) + 1;
+     if (randomizedGuess === 1) {
+         computerGuess = "rock";
+     } else if (randomizedGuess === 2) {
+         computerGuess = "paper";
+     } else if (randomizedGuess === 3) {
+         computerGuess = "scissors";
+     }
+     return computerGuess;
+ }
+
+ function humanPlay() {
+     let guessed = false;
+     let humanGuessInput;
+     while (guessed === false) {
+         humanGuessInput = prompt("Choose your weapon!", "");
+         humanGuessInput = humanGuessInput.toLowerCase();
+         if (humanGuessInput === "rock" || humanGuessInput === "paper" || humanGuessInput === "scissors") {
+             humanGuess = humanGuessInput;
+             return humanGuess;
+             guessed = true;
+         } 
+     }   
+ }
+
+ function roundPlay(humanGuess, computerGuess) {
+     if (humanGuess === computerGuess) {
+         console.log("It's a tie! Play again");
+     } else if (humanGuess === "rock" && computerGuess === "paper") {
+         console.log("You lose!");
+         computerScore += 1;
+     } else if (humanGuess === "rock" && computerGuess === "scissors") {
+         console.log("You win!");
+         humanScore += 1;
+     } else if (humanGuess === "paper" && computerGuess === "rock") {
+         console.log("You win!");
+         humanScore += 1;
+     } else if (humanGuess === "paper" && computerGuess === "scissors") {
+         console.log("You lose!");
+         computerScore += 1;
+     } else if (humanGuess === "scissors" && computerGuess === "rock") {
+         console.log("You lose!");
+         computerScore += 1;
+     } else if (humanGuess === "scissors" && computerGuess === "paper") {
+         console.log("You win!");
+         humanScore += 1;
+     }
+ }
+
+
+ function game() {
+ humanPlay();
+ computerPlay();
+ roundPlay(humanGuess, computerGuess);
+ console.log(humanScore);
+ console.log(computerScore);
+ if (humanScore === 5 || computerScore === 5) {
+     winner = true;
+     return winner;
+ }
+ }
+
+ while (winner === false) {
+ game();
+ }
+
+ if (humanScore === 5) {
+     console.log("With 5 points, you are the ultimate winner - congratulations!");
+ } else if (computerScore === 5) {
+     console.log("The computer scored 5 points - you are the ultimate loser!");
+ }
+
+const buttons = document.querySelector('button');
+buttons.forEach((button =>
+button.addEventListener('mouseover', () => {
+    button.classList.add(button-hover);
+})
+)
